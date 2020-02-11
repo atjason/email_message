393afc342a264c1ee58b0e33892bad4ccfc2a222
Jason <quietjason@gmail.com>
2020-02-12 06:07:31 +0800

    Update config_demo.js

---
+++ config_demo.js
@@ -0,0 +1,13 @@
+const config = {
+  secret: 'SECRET',
+  email: {
+    host: "smtp.gmail.com",
+    port: 465,
+    user: "ABC@gmail.com",
+    pass: "PASSWORD",
+    toEmail: "123@gmail.com"
+  },
+  port: 3300,
+}
+
+module.exports = config
